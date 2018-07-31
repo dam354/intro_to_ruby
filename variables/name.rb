1c9b2e459d509b7afff6019c3d1340bc975d9efc
Dan <dmennella34@gmail.com>
2018-07-31 08:14:57 -0400

    Variables

---
+++ variables/name.rb
@@ -0,0 +1,12 @@
+# puts "Whats your name?"
+# name = gets.chomp
+
+# puts "welcome #{name}"
+
+puts "Whats your first name?"
+first_name = gets.chomp
+
+puts "whats your last name?"
+last_name = gets.chomp
+
+puts "your name is #{first_name} #{last_name}"
